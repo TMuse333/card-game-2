6004777c440efbd7ff23d83152ee1dc9023eca73
Thomas Musial <th896493@dal.ca>
2024-02-18 13:55:24 -0400

    working on rendering the card game

---
+++ .history/src/components/clock/clock_20240218134053.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react';
+import { useGameContext } from '../context';
+
+const Clock = () => {
+  const [countdown, setCountdown] = useState(10);
+
+  const [countDownStarted, setCountdownStarted] = useState(false)
+
+  const {gameStarted} = useGameContext()
+
+
+
+  useEffect(() => {
+    setCountdownStarted(gameStarted);
+// console.log('clock is smart',)
+  }, [gameStarted]);
+
+  useEffect(() => {
+    let intervalId;
+  
+    if (gameStarted) {
+    //   console.log('lets get it started!');
+      setCountdown(10); // Start from 10
+  
+      intervalId = setInterval(() => {
+        setCountdown((prevCount) => (prevCount > 1 ? prevCount - 1 : 10)); // Decrement instead of increment
+      }, 1150);
+    }
+  
+    // Clear the interval when the component unmounts or when the game stops
+    return () => clearInterval(intervalId);
+  }, [gameStarted]);
+  
+  
+
+  return <div className="clock-container">
+    <div className='small-clock'>
+        {countdown}
+    </div>
+    <div className='big-clock'>
+        60
+    </div>
+    <div className='total-score'>
+        Score
+    </div>
+    <div className='score'>
+        1 &
+    </div>
+  </div>;
+};
+
+export default Clock;
