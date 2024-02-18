48c304b8c3ac9698c9e2ff974de52a34556a6035
Thomas Musial <th896493@dal.ca>
2024-02-18 11:18:41 -0400

    card selection is working, just need to figure out why random card isnt switching

---
+++ .history/src/components/context_20240218102943.jsx
@@ -0,0 +1,87 @@
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import charizard from '../media/charizard.gif'
+import deoxys from '../media/deoxys.gif'
+import gengar from '../media/gengar.gif'
+import mewtwo from '../media/mewtwo.gif'
+import pikachu from '../media/pikachu.gif'
+import rayquaza from '../media/rayquaza.gif'
+import sudowudo from '../media/sudowudo.gif'
+import blazekin from '../media/blazekin.gif'
+const GameContext = createContext();
+
+export const GameProvider = ({ children }) => {
+  const shuffleCards = () => {
+    return Array.from({ length: 8 }, (_, index) => index)
+      .sort(() => Math.random() - 0.5);
+  };
+
+  const getRandomNumber = () => {
+    return Math.floor(Math.random() * 8);
+  };
+
+
+
+  const cards = [
+    mewtwo,
+    deoxys, charizard,
+    sudowudo,
+    gengar,
+    pikachu,
+    rayquaza,
+    blazekin,
+  ];
+
+  const [randomNumber, setRandomNumber] = useState(getRandomNumber);
+  const [shuffledIndexes, setShuffledIndexes] = useState(shuffleCards());
+  const [gameStarted, setGameStarted] = useState(false);
+
+
+  const [randomCard, setRandomCard] = useState(cards[randomNumber] )
+
+useEffect(()=>{
+  if(gameStarted){
+    getRandomNumber()
+    console.log('game started')
+    console.log('the random card is',randomCard,randomNumber)
+    setRandomCard(cards[randomNumber]);
+
+  }
+},[gameStarted])
+
+ 
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+     
+      setShuffledIndexes(shuffleCards());
+     get
+      console.log('the random card is',randomCard,randomNumber)
+    }, 11000);
+
+    // Clear the interval when the component unmounts
+    return () => clearInterval(intervalId);
+  }, [gameStarted,getRandomNumber,randomCard]);
+
+  const contextValue = {
+    shuffleCards,
+    getRandomNumber,
+    randomNumber,
+    shuffledIndexes,
+    gameStarted,
+    setGameStarted,
+    randomCard,
+  };
+  return (
+    <GameContext.Provider value={contextValue}>
+      {children}
+    </GameContext.Provider>
+  );
+};
+
+export const useGameContext = () => {
+  const context = useContext(GameContext);
+  if (!context) {
+    throw new Error('useGameContext must be used within a GameProvider');
+  }
+  return context;
+};
---
+++ .history/src/components/context_20240218104941.jsx
@@ -0,0 +1,98 @@
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import charizard from '../media/charizard.gif'
+import deoxys from '../media/deoxys.gif'
+import gengar from '../media/gengar.gif'
+import mewtwo from '../media/mewtwo.gif'
+import pikachu from '../media/pikachu.gif'
+import rayquaza from '../media/rayquaza.gif'
+import sudowudo from '../media/sudowudo.gif'
+import blazekin from '../media/blazekin.gif'
+const GameContext = createContext();
+
+export const GameProvider = ({ children }) => {
+  const shuffleCards = () => {
+    return Array.from({ length: 8 }, (_, index) => index)
+      .sort(() => Math.random() - 0.5);
+  };
+
+  const getRandomNumber = () => {
+    return Math.floor(Math.random() * 8);
+  };
+
+
+
+  const cards = [
+    mewtwo,
+    deoxys, charizard,
+    sudowudo,
+    gengar,
+    pikachu,
+    rayquaza,
+    blazekin,
+  ];
+
+  const [randomNumber, setRandomNumber] = useState(getRandomNumber);
+  const [shuffledIndexes, setShuffledIndexes] = useState([0,1,2,3,4,5,6,7]);
+  const [gameStarted, setGameStarted] = useState(false);
+
+
+  const [randomCard, setRandomCard] = useState(cards[randomNumber] )
+
+// useEffect(()=>{
+//   if(gameStarted){
+//     getRandomNumber()
+//     console.log('game started')
+//     console.log('the random card is',randomCard,randomNumber)
+//     setRandomCard(cards[randomNumber]);
+
+//   }
+// },[gameStarted,randomNumber])
+
+useEffect(()=>{
+
+  setRandomNumber(getRandomNumber)
+  console.log('initial random card is',cards[randomNumber])
+},[])
+
+ 
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+     
+      if(gameStarted){
+        // setShuffledIndexes(shuffleCards());
+   
+        setRandomNumber(  getRandomNumber())
+        setRandomCard(cards[randomNumber])
+         console.log('the random card is',randomCard,randomNumber)
+      }
+   
+    }, 5000);
+
+    // Clear the interval when the component unmounts
+    return () => clearInterval(intervalId);
+  }, [gameStarted,getRandomNumber,randomCard,randomNumber]);
+
+  const contextValue = {
+    shuffleCards,
+    getRandomNumber,
+    randomNumber,
+    shuffledIndexes,
+    gameStarted,
+    setGameStarted,
+    randomCard,
+  };
+  return (
+    <GameContext.Provider value={contextValue}>
+      {children}
+    </GameContext.Provider>
+  );
+};
+
+export const useGameContext = () => {
+  const context = useContext(GameContext);
+  if (!context) {
+    throw new Error('useGameContext must be used within a GameProvider');
+  }
+  return context;
+};
