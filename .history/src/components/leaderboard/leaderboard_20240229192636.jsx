d9c41b5b8464168e0db2eedc088329a40dccea19
Thomas Musial <th896493@dal.ca>
2024-02-29 19:26:53 -0400

    closed window

--- .history/src/components/leaderboard/leaderboard_20240229192636.jsx
+++ .history/src/components/leaderboard/leaderboard_20240229192636.jsx
@@ -23,7 +23,7 @@ const Leaderboard = () => {
         setTotalScore(0);
       })
       .catch(error => {
-        setLeaderboardSelected(false)
+        
         window.alert("Download the game at https://github.com/TMuse333/card-game-2 to view the leader board!")
         console.error('Error fetching leaderboard data', error);
       });
