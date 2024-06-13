a2d88c39c93511a3eafb725c185db8c3d58da104
Thomas Musial <th896493@dal.ca>
2024-06-13 15:58:09 -0300

    not a multiple routes moment for now

--- server/index.js
+++ server/index.js
@@ -1,28 +1,35 @@
+// index.js
+
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
-import cors from 'cors';
-import userRoutes from './routes/user.routes.js';  // Import user routes
-import leaderboardRoutes from './routes/leaderboard.routes.js';  // Import leaderboard routes
+import routes from './routes.js'
+
+import cors from 'cors'
+
+
 
 const app = express();
+app.use(cors());
 dotenv.config();
 
 mongoose.connect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log('MongoDB connected');
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
-  });
+    .then(() => {
+        console.log('MongoDB connected');
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error);
+    });
 
-app.use(cors());
-app.use(express.json());
+    app.use(express.json());
 
-// Use the routes
-app.use('/users', userRoutes);
-app.use('/leaderboard', leaderboardRoutes);
+
+    app.use('/', routes); 
+    
+
+
+  
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+    console.log('Server is running on port 9000');
 });
