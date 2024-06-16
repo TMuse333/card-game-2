5bb4c9af20d32c2bd4816c1f04f22cebf84deec7
Thomas Musial <th896493@dal.ca>
2024-06-16 15:55:14 -0300

    node cheesing

--- server/emailsender.js
+++ server/emailsender.js
@@ -10,7 +10,7 @@ const password = process.env.GMAIL_PASSWORD
 const transporter = nodemailer.createTransport({
   service: 'Gmail', // Use your email service provider
   auth: {
-    user: 'your_email@gmail.com', // Your email address
+    user: 'q3visualdesigns@gmail.com', // Your email address
     pass: password, // Your email password or application-specific password
   },
 });
